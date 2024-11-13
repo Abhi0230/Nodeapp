e440f8db682d706ee1002b1921bb30399c5e12b8
abhay <abhayvishwakarma800@gmail.com>
2024-11-13 10:19:24 +0530

    test

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 var request = require('supertest');
-var app = require('../index.js');describe('GET /', function() {
- it('respond with hello world', function(done) { //navigate to root and check the the response is "hello world"
+var app = require('../index.js');
+describe('GET /', function() {
+ it('respond with hello Tushar', function(done) { //navigate to root and check the the response is "hello Tushar"
  request(app).get('/').expect('hello Tushar', done);
  });
 });
